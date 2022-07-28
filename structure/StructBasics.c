6a6cb9f6c9219da5f16e2fb732b69adef2e16bb1
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-07-28 12:33:21 +0530

    fix code

--- structure/StructBasics.c
+++ structure/StructBasics.c
@@ -9,8 +9,8 @@
     {
 
         printf("Enter Book Name, Price and No of Pages.\n");
-        scanf("%s %f %d", &b.name, &b.price, &b.pages);
+        scanf("%s %f %d", &b1.name, &b1.price, &b1.pages);
         printf("And this is what you entered .\n");
-        printf("%s %.2f %d", bp->name, bp->price, bp->pages);
+        printf("%s %.2f %d", b1.name, b1.price, b1.pages);
         return 0;
     }
