97e53aa171d458bd7fd2a5c5212a0e661eba9d32
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-07-28 12:11:00 +0530

    Add files via upload

---
+++ structure/StructArray.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+    struct book
+    {
+        char name[20];
+        float price;
+        int pages;
+    } b[5];
+    int main()
+    {
+        int i;
+        for (i = 0; i <= 2; i++)
+        {
+            printf("Enter Book Name, Price and No of Pages for book no:  %d.\n ",i+1);
+            scanf("%s %f %d", &b[i].name, &b[i].price, &b[i].pages);
+        }
+        printf("And this is what you entered .\n");
+        for (i = 0; i <= 2; i++)
+        {
+            printf("Book: %d %s %.2f %d\n", i+1, b[i].name, b[i].price, b[i].pages);
+        }
+    return 0;
+    }
