9edce0fa8a5171df369801928525b162d968aecf
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-07-28 12:09:54 +0530

    Create StructBasics.c

---
+++ structure/StructBasics.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+    struct book
+    {
+        char name[20];
+        float price;
+        int pages;
+    } b1;
+    int main()
+    {
+
+        printf("Enter Book Name, Price and No of Pages.\n");
+        scanf("%s %f %d", &b.name, &b.price, &b.pages);
+        printf("And this is what you entered .\n");
+        printf("%s %.2f %d", bp->name, bp->price, bp->pages);
+        return 0;
+    }
