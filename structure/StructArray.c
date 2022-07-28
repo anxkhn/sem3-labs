63f3ac6cb576eb4d8996cc4fd5f702468cd7842c
Ninad Naik <86565419+ninadnaik10@users.noreply.github.com>
2022-07-28 20:21:56 +0530

    Make places for 5 books

--- structure/StructArray.c
+++ structure/StructArray.c
@@ -8,13 +8,13 @@
     int main()
     {
         int i;
-        for (i = 0; i <= 2; i++)
+        for (i = 0; i < 5; i++)
         {
             printf("Enter Book Name, Price and No of Pages for book no:  %d.\n ",i+1);
-            scanf("%s %f %d", &b[i].name, &b[i].price, &b[i].pages);
+            scanf("%s %f %d", b[i].name, &b[i].price, &b[i].pages);
         }
         printf("And this is what you entered .\n");
-        for (i = 0; i <= 2; i++)
+        for (i = 0; i < 5; i++)
         {
             printf("Book: %d %s %.2f %d\n", i+1, b[i].name, b[i].price, b[i].pages);
         }
