035161d9e7e9c0d7771d8e609d3c7383c0b04088
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-07-30 14:58:33 +0530

    day 0 lab

---
+++ dsa/01-StructBasics.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+struct book
+{
+    char name[20];
+    float price;
+    int pages;
+} b1;
+int main()
+{
+
+    printf("Enter Book Name, Price and No of Pages.\n");
+    scanf("%s %f %d", &b1.name, &b1.price, &b1.pages);
+    printf("And this is what you entered .\n");
+    printf("%s %.2f %d", b1.name, b1.price, b1.pages);
+    return 0;
+}
