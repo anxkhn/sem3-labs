4e83163ea2e856f39667376fe759c7780398aa8a
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-10-29 13:55:45 +0530

    update code : fix peak and use stdlib.h

--- dsa/05-StackUsingArray.c
+++ dsa/05-StackUsingArray.c
@@ -1,70 +1,86 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+// using stdlib.h for exit(0) function
+
 #define SIZE 10
 
-struct stack {
+struct stack
+{
     int items[SIZE];
     int top;
-}s1;
+} s1;
 
-void push() {
-
-    if (s1.top == SIZE - 1) {
+void push()
+{
+    if (s1.top == SIZE - 1)
+    {
         printf("Stack overflow.\n");
-    } 
-	else {
-        int x;
+    }
+    else
+    {
         printf("Enter a number: ");
-        scanf("%d", & x);
+        int x;
+        scanf("%d", &x);
         s1.top++;
         s1.items[s1.top] = x;
-        return;
     }
+    return;
 }
 
-void pop() {
-    if (s1.top == -1) {
+void pop()
+{
+    if (s1.top == -1)
+    {
         printf("Stack underflow.\n");
-        return;
-    } 
-	else {
+    }
+    else
+    {
         printf("Deleted item: %d\n", s1.items[s1.top]);
         s1.top--;
     }
     return;
 }
 
-void display() {
-    if (s1.top == -1) {
+void display()
+{
+    if (s1.top == -1)
+    {
         printf("Stack is empty.\n");
         return;
-    } 
-	else {
-        for (int i = s1.top; i >= 0; i--) {
+    }
+    else
+    {
+        for (int i = s1.top; i >= 0; i--)
+        {
             printf("%d\n", s1.items[i]);
         }
     }
     return;
 }
 
-void peek() {
-    if (s1.top == 0) {
+void peek()
+{
+    if (s1.top == -1)
+    {
         printf("Stack is empty.\n");
-        return;
-    } 
-	else {
-
+    }
+    else
+    {
         printf("%d\n", s1.items[s1.top]);
     }
+    return;
 }
 
-void main() {
-    int flag = 1;
+void main()
+{
     s1.top = -1;
     int n;
-    while (flag) {
+    while (1)
+    {
         printf("Enter you choice\n1. Push\n2. Pop\n3. Display\n4. Peek\n5. Exit\n");
-        scanf("%d", & n);
-        switch (n) {
+        scanf("%d", &n);
+        switch (n)
+        {
         case 1:
             push();
             break;
@@ -79,7 +95,7 @@ void main() {
             break;
         case 5:
             printf("Thank You!\n");
-            flag = 0;
+            exit(0);
             break;
         default:
             printf("Bad input.\n");
