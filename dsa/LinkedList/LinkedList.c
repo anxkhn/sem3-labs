e21ad39e9ec30d116f7dd6c2025394c178f09782
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-10-29 22:41:46 +0530

    add comments

--- dsa/LinkedList/LinkedList.c
+++ dsa/LinkedList/LinkedList.c
@@ -13,21 +13,27 @@ struct node *insert(struct node *head, int num)
     struct node *temp;
     temp = head;
     struct node *new = (struct node *)malloc(sizeof(struct node));
+  //initializing a new node to be inserted
     new->data = num;
     new->next = NULL;
-    // insert at first conditions
+//list is empty or smallest element then insert at beginning
     if (temp == NULL || head->data >= num)
     {
         new->next = head;
         head = new;
     }
-    // other positions insert
+  
     else
-    {
+    { 
+//the node youre at isnt the last node (next not null) and
+//data is greater than current node youre at, keep moving
+      //shortcircuit condition because you don't want to access memeory which does not exist
         while (temp->next != NULL && temp->next->data < num)
         {
             temp = temp->next;
         }
+//you stop moving and insert when data is just greater than current node
+//or last element reached
         new->next = temp->next;
         temp->next = new;
     }
@@ -44,11 +50,14 @@ struct node *del(struct node *head, int num)
         // return head;
     }
     else
+      //list not empty
     {
         // delete at first
         if (temp->data == num)
         {
+            //assign node 2 as head
             head = temp->next;
+          // freeing temp's mem
             free(temp);
             // return head;
         }
@@ -62,12 +71,16 @@ struct node *del(struct node *head, int num)
                 {
                     node2del = temp->next;
                     temp->next = temp->next->next;
+                  //malloc heap mem
                     free(node2del);
                     check = 1;
                     return head;
                 }
+             //if case failed travel aage
+             //skskskskskksks
                 temp = temp->next;
             }
+         
             if (check == 0)
             {
                 printf("Element not found\n");
@@ -77,23 +90,30 @@ struct node *del(struct node *head, int num)
     }
     return head;
 }
-
+// display fn takes in head
 void display(struct node *head)
 {
     struct node *temp;
     temp = head;
+  //temp which is storing head is empty, list is empty
     if (temp == NULL)
     {
         printf("EMPTY LL\n");
-        return;
+
     }
-    printf("---------------------\n");
-    while (temp != NULL)
+    // sins head is not pointing to null it will go to else block
+    else
+    {
+    // this loop will keep checking till temp is null ie list not over
+    while (temp!= NULL)
     {
         printf("%d->", temp->data);
+      // update temp to next node
         temp = temp->next;
+      // last node's next will be null 
     }
-    printf("NULL\n---------------------");
+    printf("NULL\n");
+  }
 }
 void main()
 {
